a6dff7f109b38ab3509adc1eaa5d52b52d14084f
Pushkar Kashyapi <141157548+PushkarKashyapi@users.noreply.github.com>
2026-08-23 23:52:21 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,7 @@
-"""
-Main URL configuration for the Social Media Content Analyzer backend.
-"""
-
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    
     path("admin/", admin.site.urls),
-
-
     path("api/v1/", include("analyzer.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
