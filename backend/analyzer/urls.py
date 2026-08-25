b9f6811de7d87d582bed6cce04152a9cf1578f91
Pushkar Kashyapi <141157548+PushkarKashyapi@users.noreply.github.com>
2026-08-25 10:49:02 +0530

    Add health endpoint to URLs

--- backend/analyzer/urls.py
+++ backend/analyzer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-
 from analyzer.views.analyze_view import AnalyzeView
+from analyzer.views.health_view import health
 
 urlpatterns = [
-    
     path("analyze/", AnalyzeView.as_view(), name="analyze"),
+    path("health/", health, name="health"),
 ]
